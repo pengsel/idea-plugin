c0ce1e9d599d72a11118e1bc85e2d60e35b203ee
GitHub Action <action@github.com>
2021-10-03 03:02:01 +0000

    Template cleanup

--- src/main/kotlin/com/github/pengsel/ideaplugin/MyBundle.kt
+++ src/main/kotlin/com/github/pengsel/ideaplugin/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.pengsel.ideaplugin
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/pengsel/ideaplugin/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/pengsel/ideaplugin/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.pengsel.ideaplugin.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.pengsel.ideaplugin.MyBundle
 
 class MyApplicationService {
 
